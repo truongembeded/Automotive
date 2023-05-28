13a908c5e26aa72abbe361032550cf534724e494
Truong <nguyenvantruong2010cdt@gmail.lcom>
2023-05-28 22:36:42 +0700

    Led13

---
+++ BlinkLED/Dio.c
@@ -0,0 +1,69 @@
+#include "Dio.h"
+
+void Dio_WriteChannle(Dio_ChannleType Channle1Id, Dio_levelType Level){
+		GPIO_TypeDef * gpioPort;
+		uint16_t gpioPin;
+	
+		switch(Channle1Id){
+			case DIO_CHANNLE_PA0:
+				gpioPort = GPIOA;
+				gpioPin = GPIO_Pin_0;
+				break;
+			case DIO_CHANNLE_PA1:
+				gpioPort = GPIOA;
+				gpioPin = GPIO_Pin_1;
+				break;
+			case DIO_CHANNLE_PB1:
+				gpioPort = GPIOB;
+				gpioPin = GPIO_Pin_1;
+				break;
+			case DIO_CHANNLE_PC1:
+				gpioPort = GPIOC;
+				gpioPin = GPIO_Pin_1;
+				break;
+			case DIO_CHANNLE_PC13:
+				gpioPort = GPIOC;
+				gpioPin = GPIO_Pin_13;
+				break;
+		}
+		if(Level == STD_LOW){
+			GPIO_ResetBits(gpioPort, gpioPin);
+		}else{
+			GPIO_SetBits(gpioPort, gpioPin);
+		}
+}
+
+Dio_levelType Dio_ReadChannle(Dio_ChannleType Channle1Id){
+		GPIO_TypeDef * gpioPort;
+		uint16_t gpioPin;
+		switch(Channle1Id){
+			case DIO_CHANNLE_PA0:
+				gpioPort = GPIOA;
+				gpioPin = GPIO_Pin_0;
+				break;
+			case DIO_CHANNLE_PA1:
+				gpioPort = GPIOA;
+				gpioPin = GPIO_Pin_1;
+				break;
+			case DIO_CHANNLE_PB1:
+				gpioPort = GPIOB;
+				gpioPin = GPIO_Pin_1;
+				break;
+			case DIO_CHANNLE_PC1:
+				gpioPort = GPIOC;
+				gpioPin = GPIO_Pin_1;
+				break;
+			case DIO_CHANNLE_PC13:
+				gpioPort = GPIOC;
+				gpioPin = GPIO_Pin_13;
+				break;
+		}default:;
+		
+		if(GPIO_ReadInputDataBit(gpioPort, gpioPin) == STD_LOW){
+			return STD_LOW;
+		}else{
+			return STD_HIGH;
+		}
+
+}
+
---
+++ BlinkLED/Dio.h
@@ -0,0 +1,22 @@
+#include "stm32f10x.h"
+
+#ifndef __DIO_H
+#define __DIO_H
+typedef enum{
+	DIO_CHANNLE_PA0,
+	DIO_CHANNLE_PA1,
+	DIO_CHANNLE_PB1,
+	DIO_CHANNLE_PC1,
+	DIO_CHANNLE_PC13
+}Dio_ChannleType;
+
+typedef enum{
+	STD_LOW,
+	STD_HIGH
+}Dio_levelType;
+
+void Dio_WriteChannle(Dio_ChannleType Channle1Id, Dio_levelType Level);
+
+Dio_levelType Dio_ReadChannle(Dio_ChannleType Channle1Id);
+
+#endif
